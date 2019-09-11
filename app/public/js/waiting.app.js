a8ede8032590c2336be4eed5bff464066265a130
feliciaclarissa <fclariss@iu.edu>
2019-09-10 22:33:10 -0400

    Vue Js change based on Tom's video

---
+++ app/public/js/waiting.app.js
@@ -0,0 +1,28 @@
+var waitingApp = new Vue({
+  el: '#patientWaitingApp',
+  data: {
+  patients: [
+  {
+      "patientGuid":1,
+      "firstName":"Edgar",
+      "lastName":"Truran",
+      "dob":"2/17/2019",
+      "sexAtBirth":"Male"
+  },
+  {
+    "patientGuid":2,
+    "firstName":"Gianni",
+    "lastName":"Berka",
+    "dob":"9/13/2018",
+    "sexAtBirth":"Male"
+  },
+  {
+    "patientGuid":3,
+    "firstName":"Tab",
+    "lastName":"Widger",
+    "dob":"12/28/2018",
+    "sexAtBirth":"Male"
+  },
+]
+}
+});
