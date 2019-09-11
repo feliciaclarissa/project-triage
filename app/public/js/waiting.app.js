7c0e2dcacb6daca8e364e59d3124f58ed99c96d7
feliciaclarissa <fclariss@iu.edu>
2019-09-11 15:16:14 -0400

    fetch dummy data 9/11/19

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,28 +1,17 @@
 var waitingApp = new Vue({
   el: '#patientWaitingApp',
-  data: {
-  patients: [
-  {
-      "patientGuid":1,
-      "firstName":"Edgar",
-      "lastName":"Truran",
-      "dob":"2/17/2019",
-      "sexAtBirth":"Male"
-  },
-  {
-    "patientGuid":2,
-    "firstName":"Gianni",
-    "lastName":"Berka",
-    "dob":"9/13/2018",
-    "sexAtBirth":"Male"
-  },
-  {
-    "patientGuid":3,
-    "firstName":"Tab",
-    "lastName":"Widger",
-    "dob":"12/28/2018",
-    "sexAtBirth":"Male"
-  },
-]
+  data: {           //*variable that we use
+    patients: []
+},
+  methods: {
+  fetchPatients () {         /*assign function to varialbe*/
+    fetch('dummy.php')
+    .then(response => response.json())
+    .then(json => {waitingApp.patients = json});
 }
+},
+created() {                 //*define code
+  this.fetchPatients();
+}
+
 });
