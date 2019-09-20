cc12ab60f6aac0cb27409cedc215acf01a9ed40b
feliciaclarissa <fclariss@iu.edu>
2019-09-20 14:26:07 -0400

    v-model and events

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,13 +1,32 @@
-var recordApp = new Vue({
+var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {           //*variable that we use
-    patients: []
+    patients: [],
+    formPatient ={
+      firstName: '',
+      lastName:'',
+      dob:'',
+      sexAtBirth:'',
+    }
 },
   methods: {
   fetchPatients () {         /*assign function to varialbe*/
     fetch('dummy.php')
     .then(response => response.json())
-    .then(json => {recordApp.patients = json});
+    .then(json => {patientRecordApp.patients = json})
+
+},
+handleCreateRecord(event){
+  this.patients.push(this.formPatient);
+  this.formPatient ={
+    firstName: '',
+    lastName:'',
+    dob:'',
+    sexAtBirth:'',
+}
+},
+handleRecordClick(patient){
+  console.log(patient);
 }
 },
 created() {                 //*define code
